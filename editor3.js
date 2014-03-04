ee737943d39affec5d6ad49748226072234c5684
Elijah Insua <tmpvar@gmail.com>
2014-03-04 15:09:46 -0800

    baseline Editor3 and Editor3Mesh

--- editor3.js
+++ editor3.js
@@ -1,12 +1,140 @@
-var editor3 = function() {
-  
+if (typeof require !== 'undefined') {
+  var THREE = require('three');
 }
+
+
+
+function Editor3(selector, scene, renderer, camera) {
+
+  this.updateSteps = [];
+
+  this.scene = new THREE.Scene();
+
+  this.camera = new THREE.PerspectiveCamera(70, 4/3, 1, 1000);
+  this.camera.position.set( 0, 10, 40 );
+  this.scene.add(camera);
+
+  this.renderer = renderer || new THREE.WebGLRenderer({
+    antialias : true,
+    stencil: true,
+    preserveDrawingBuffer: true
+  });
+
+  var light = new THREE.HemisphereLight( 0xffffff, 0x222225, .6);
+  light.position = this.camera.position;
+  this.scene.add( light );
+
+  this.container = document.querySelector(selector)
+  this.container.appendChild( this.renderer.domElement );
+
+  this.resize();
+  window.addEventListener('resize', this.resize.bind(this), false);
+  this.renderer.setClearColor(0x222225);
+};
+
+Editor3.prototype.resize = function() {
+  var w = this.container.offsetWidth;
+  var h = this.container.offsetHeight;
+
+  this.renderer.setSize(w, h);
+  this.camera.aspect = w/h;
+  this.camera.updateProjectionMatrix();
+}
+
+Editor3.prototype.lastRender = 0;
+Editor3.prototype.render = function(t) {
+
+  var updateSteps = this.updateSteps;
+  var d = t - this.lastRender;
+  this.lastRender = t;
+
+  this.renderer.render(this.scene, this.camera);
+
+  for (var i = 0; i<updateSteps.length; i++) {
+    if (typeof updateSteps[i].update === 'function') {
+      updateSteps[i].update(d);
+    } else if (typeof updateSteps[i] === 'function') {
+      updateSteps[i](d);
+    }
+  }
+};
+
+Editor3.prototype.createMesh = function() {
+  return new Editor3.Mesh();
+};
+
+Editor3.prototype.addMesh = function(mesh) {
+  this.scene.add(mesh);
+};
+
+Editor3.prototype.removeMesh = function(mesh) {
+  this.scene.remove(mesh);
+};
+
+function Editor3Mesh(geometry, material) {
+  THREE.Mesh.call(this,
+    geometry || new THREE.Geometry(),
+    material || this.defaultMaterial
+  );
+
+  this.seenVerts = {};
+};
+
+Editor3Mesh.prototype = Object.create(THREE.Mesh.prototype);
+
+Editor3Mesh.prototype.defaultMaterial = new THREE.MeshPhongMaterial({
+  color: 0xffffff,
+  emissive: 0x020202,
+  ambient: 0x111111,
+  shading: THREE.SmoothShading,
+  shinyness: 100
+});
+
+Editor3Mesh.prototype.addFace = function(verts, normal) {
+  var seen = this.seenVerts;
+  var geometry = this.geometry;
+
+  var faceIds = verts.map(function(vert) {
+    var key = vert.join(',');
+
+    if (!seen[key]) {
+      seen[key] = geometry.vertices.length;
+      geometry.vertices.push(new THREE.Vector3(
+        vert[0],
+        vert[1],
+        vert[2]
+      ));
+    }
+
+    return seen[key];
+  });
+
+  this.geometry.faces.push(new THREE.Face3(
+    faceIds[0],
+    faceIds[1],
+    faceIds[2],
+    new THREE.Vector3(
+      normal[0],
+      normal[1],
+      normal[2]
+    )
+  ));
+
+  return this;
+};
+
+Editor3Mesh.prototype.finalize = function() {
+  this.geometry.computeBoundingBox();
+  return this;
+};
+
+Editor3.Mesh = Editor3Mesh;
 
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = editor3;
+  module.exports = Editor3;
 }
 
 if (typeof window !== "undefined") {
-  window.editor3 = window.editor3 || editor3;
+  window.Editor3 = window.Editor3 || Editor3;
 }
